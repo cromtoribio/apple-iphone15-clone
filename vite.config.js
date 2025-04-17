395a348bef8b7c52b3a37118c8b24b2f3af4070c
Chris Toribio <44413949+cromtoribio@users.noreply.github.com>
2025-04-17 11:16:01 -0700

    Fixed root indicator

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-    root: "src/",
+    root: "/",
     base: process.env.NODE_ENV === "production" ? "/apple-iphone15-clone" : "",
     publicDir: "../public/",
     plugins: [react(), tailwindcss(), sentryVitePlugin({
